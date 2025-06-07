550e3a56ed8c3f73ea7d7088e751313972f3961c
Jeremy <jeremy@MacBook-Air-de-Jeremy.local>
2025-06-07 11:07:58 +0200

    arc worksgit add -A

---
+++ external/arc_angle_updater/arc_angle_updater.cpp
@@ -0,0 +1,9 @@
+#include "arc_angle_updater.h"
+
+namespace esphome {
+namespace arc_angle_updater {
+
+// No additional implementation needed
+
+}  // namespace arc_angle_updater
+}  // namespace esphome
