6dba95f5b8d7d362494db42f57d60130d37a7c25
Jeremy <jeremy@MacBook-Air-de-Jeremy.local>
2025-06-07 11:19:08 +0200

    icon works

--- external/arc_angle_updater/arc_angle_updater.cpp
+++
@@ -1,9 +0,0 @@
-#include "arc_angle_updater.h"
-
-namespace esphome {
-namespace arc_angle_updater {
-
-// No additional implementation needed
-
-}  // namespace arc_angle_updater
-}  // namespace esphome
